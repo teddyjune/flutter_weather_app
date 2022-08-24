cf6fa09e9db32fd3022caeaf5ea64f974be8ff8a
yeonjoo.lee <teddyjune@naver.com>
2022-08-24 19:18:41 +0900

    feat:OpenWeatherApiImpl test 코드 작성

--- lib/data/data_source/open_weather_api_impl.dart
+++ lib/data/data_source/open_weather_api_impl.dart
@@ -5,10 +5,15 @@ import 'package:weather_app/data/data_source/open_weather_api.dart';
 import 'package:weather_app/data/data_source/weather_dto.dart';
 
 class OpenWeatherApiImpl implements OpenWeatherApi {
+  final http.Client _client;
+
+  OpenWeatherApiImpl({http.Client? client})
+      : _client = (client ?? http.Client());
+
   Future<WeatherDto> getWeatherInfo(String query) async {
     Uri url = Uri.parse(
         'https://api.openweathermap.org/data/2.5/weather?q=$query&appid=95114a1e948559e010396b4debdf1672');
-    http.Response response = await http.get(url);
+    http.Response response = await _client.get(url);
 
     String jsonString = response.body;
     Map<String, dynamic> json = jsonDecode(jsonString);
--- test/data/data_source/open_weather_api_impl_test.dart
+++ test/data/data_source/open_weather_api_impl_test.dart
@@ -1,6 +1,8 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart';
+import 'package:http/testing.dart';
 import 'package:weather_app/data/data_source/open_weather_api_impl.dart';
+import 'package:weather_app/data/data_source/weather_dto.dart';
 
 void main() {
   test('날씨 정보를 제대로 가져오는지 체크', () async {
@@ -11,6 +13,55 @@ void main() {
       }
       return Response('error', 404);
     });
-    final api = OpenWeatherApiImpl(api);
+    final api = OpenWeatherApiImpl(client: mockClient);
+    WeatherDto weather = await api.getWeatherInfo('seoul');
+    expect(weather.name, 'Seoul');
+    expect(weather.weather![0].main, 'Clouds');
   });
 }
+
+const fakeData = """
+{
+  "coord": {
+    "lon": 126.9778,
+    "lat": 37.5683
+  },
+  "weather": [
+    {
+      "id": 803,
+      "main": "Clouds",
+      "description": "broken clouds",
+      "icon": "04d"
+    }
+  ],
+  "base": "stations",
+  "main": {
+    "temp": 299.36,
+    "feels_like": 299.36,
+    "temp_min": 298.81,
+    "temp_max": 299.91,
+    "pressure": 1007,
+    "humidity": 59
+  },
+  "visibility": 10000,
+  "wind": {
+    "speed": 2.57,
+    "deg": 190
+  },
+  "clouds": {
+    "all": 75
+  },
+  "dt": 1661334441,
+  "sys": {
+    "type": 1,
+    "id": 8105,
+    "country": "KR",
+    "sunrise": 1661288118,
+    "sunset": 1661336058
+  },
+  "timezone": 32400,
+  "id": 1835848,
+  "name": "Seoul",
+  "cod": 200
+}
+""";
